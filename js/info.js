1cc87a36e57f1b736e54ce11e101f1e0394e5a3e
Bright Chung <bywoch@gmail.com>
2020-12-18 15:00:00 +0900

    upload js

---
+++ js/info.js
@@ -0,0 +1,121 @@
+﻿var infoUtil = {
+	Location: "Notice",
+	Page: 1,
+	Size: 10,
+	PageSize: 10,
+	TotalPage: 1,
+	BlockPage: 1,
+	Opt: 0,
+	Keyword: "",
+	Num: 0,
+	Idx: 0,
+	ImageServer: 'https://zzzzinfo.com/data/',
+	PageNoticeView: '/pr/notice/view.html?idx=',
+	PageNotice: '/pr/notice/index.html?page=',
+	PagePressView: '/pr/press/view.html?idx=',
+	PagePress: '/pr/press/index.html?page=',
+	PageNewsLetterView: '/pr/newsletter/view.html?idx=',
+	PageNewsLetter: '/pr/newsletter/index.html?page=',
+	UrlParameter: function (str) {
+		str = str.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+		var regex = new RegExp('[\\?&]' + str + '=([^&#]*)');
+		var results = regex.exec(location.search);
+		return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+	},
+	InitPageInfo: function (o) {
+		var _this = this;
+		_this.TotalPage = parseInt(o.total_cnt / _this.Size, 10);
+		if (_this.TotalPage * _this.Size != o.total_cnt) {
+			_this.TotalPage++;
+		}
+		_this.BlockPage = parseInt(parseInt(_this.Page - 1) / _this.PageSize) * _this.PageSize + 1;
+		_this.Num = o.total_cnt - (_this.Size * (_this.Page - 1));
+	},
+	InitPage: function () {
+		var _this = this;
+		var _keyword = infoUtil.UrlParameter("keyword");
+		if (_keyword != "") {
+			infoUtil.Keyword = _keyword;
+			$("#keyword").val(_keyword);
+		}
+		var _opt = infoUtil.UrlParameter("opt");
+		if (_opt != "") {
+			infoUtil.Opt = _opt;
+			$("#opt").val(_opt);
+		}
+
+		$("#keyword").keydown(function (key) {
+			if (key.keyCode == 13) {
+				$("#btnSearch").click();
+			}
+		});
+		$("#btnSearch").click(function () {
+			_this.Page = 1;
+			_this.Opt = $("#opt").val();
+			_this.Keyword = $.trim($("#keyword").val());
+			if (_this.Keyword.length == 0) {
+				alert("검색어를 입력해 주세요");
+				return false;
+			}
+			switch (_this.Location) {
+				case "Notice": _this.NoticeList(); break;
+				case "Press": _this.PressList(); break;
+				case "Photo": _this.PhotoList(); break;
+				case "NewsLetter": _this.NewsLetterList(); break;
+			}
+		});
+	},
+	NoticeList: function () {
+		var _this = this;
+		var _that = infoUI;
+		_this.Location = "Notice";
+		$.ajax({
+			//type: "get",
+			url: "//zzzzinfo.com/api/notice/list",
+			dataType: "jsonp",
+			async: true,
+			cache: true,
+			data: {
+				opt: _this.Opt,
+				keyword: _this.Keyword,
+				page: _this.Page,
+				size: _this.Size
+			},
+			success: function (o) {
+				_this.InitPageInfo(o);
+				_that.NoticeList(o.list);
+			},
+			error: function (request, status, error) {
+				//alert("code:"+request.status+"\n"+"message:"+request.responseText+"\n"+"error:"+error);
+			}
+		});
+	},
+	NoticeInfo: function () {
+		var _this = this;
+		var _that = infoUI;
+		var n = _this.UrlParameter("idx");
+		var _keyword = infoUtil.UrlParameter("keyword");
+		var _opt = infoUtil.UrlParameter("opt");
+		if (_opt != "") { infoUtil.Opt = _opt; }
+		if (_keyword != "") { infoUtil.Keyword = _keyword; }
+		_this.Location = "Notice";
+		_this.Page = _this.UrlParameter("page");
+		$.ajax({
+			//type: "get",
+			url: "//zzzzinfo.com/api/notice/info",
+			dataType: "jsonp",
+			async: true,
+			cache: true,
+			data: {
+				intIdx: n
+			},
+			success: function (o) {
+				_that.NoticeInfo(o);
+			},
+			error: function (request, status, error) {
+				//alert("code:"+request.status+"\n"+"message:"+request.responseText+"\n"+"error:"+error);
+			}
+		});
+	},
+}
+
---
+++ js/infoUI.js
@@ -0,0 +1,54 @@
+﻿var infoUI = {
+    NoticeList: function (o) {
+        var azHtml = [];
+        var _this = this;
+        var _that = infoUtil;
+        if (o.length > 0) {
+            for (var i = 0; i < o.length; i++) {
+                azHtml.push('<tr>');
+                azHtml.push('<td>' + (_that.Num--) + '</td>');
+                azHtml.push('<td class="td_tit">');
+                azHtml.push('<div class="in">');
+                azHtml.push('<div class="t_tit"><a href="' + _that.PageNoticeView + o[i].code + '&page=' + _that.Page + '&opt=' + _that.Opt + '&keyword=' + encodeURIComponent(_that.Keyword) + '"><strong>' + o[i].title + '</strong></a>');
+                if (o[i].is_new) {
+                    azHtml.push('<span class="sp_tinfo ico_new">new</span>');
+                }
+                azHtml.push('</div>');
+                azHtml.push('</div>');
+                azHtml.push('</td>');
+                azHtml.push('<td class="date">');
+                azHtml.push('<div class="in">' + o[i].reg_dt_short);
+                azHtml.push('</div>');
+                azHtml.push('</td>');
+                azHtml.push('</tr>');
+            }
+        } else {
+            azHtml.push('<tr><td colspan="3"><div class="in"><div class="t_tit"><strong>검색된 결과가 없습니다.</strong></div></div></td></tr>');
+        }
+        $(".sct_tblarea").find("tbody").html(azHtml.join(''));
+        _this.SetPager();
+
+    },
+    NoticeInfo: function (o) {
+        var azHtml = [];
+        var _this = this;
+        var _that = infoUtil;
+
+        $(".vtl_tit").html('<strong>' + o.info.title + '</strong>');
+        $(".view_txt").html(o.info.contents);
+        $(".vtl_date").html(o.info.reg_dt_full);
+        if (o.prev.code === null) {
+            $(".cc_ellip_in").first().html('이전 공지사항이 없습니다');
+        } else {
+            $(".page_prev").attr("href", _that.PageNoticeView + o.prev.code);
+            $(".cc_ellip_in").first().html('<a href="' + _that.PageNoticeView + o.prev.code + '&page=' + _that.Page + '">' + o.prev.title + '</a>');
+        }
+        if (o.next.code === null) {
+            $(".cc_ellip_in").last().html('다음 공지사항이 없습니다');
+        } else {
+            $(".page_next").attr("href", _that.PageNoticeView + o.next.code);
+            $(".cc_ellip_in").last().html('<a href="' + _that.PageNoticeView + o.next.code + '&page=' + _that.Page + '">' + o.next.title + '</a>');
+        }
+        $(".v_btn").attr("href", _that.PageNotice + _that.Page + '&opt=' + _that.Opt + '&keyword=' + encodeURIComponent(_that.Keyword));
+    },
+}
