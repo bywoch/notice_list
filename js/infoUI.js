bc3ff5f4e63b14500da0e5eb611dc50731624681
Bright Chung <bywoch@gmail.com>
2020-12-21 17:00:00 +0900

    update js

--- js/infoUI.js
+++ js/infoUI.js
@@ -333,3 +333,20 @@
         }
     }
 }
+
+String.prototype.cut = function (len) {
+    var str = this;
+    var l = 0;
+    for (var i = 0; i < str.length; i++) {
+        l += (str.charCodeAt(i) > 128) ? 2 : 1;
+        if (l > len) return str.substring(0, i) + "...";
+    }
+    return str;
+}
+
+String.prototype.bytes = function () {
+    var str = this;
+    var l = 0;
+    for (var i = 0; i < str.length; i++) l += (str.charCodeAt(i) > 128) ? 2 : 1;
+    return l;
+}
