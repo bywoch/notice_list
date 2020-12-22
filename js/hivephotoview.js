b7bd8efbf1d5ce66a21db25a94f6494adc0a361c
Bright Chung <bywoch@gmail.com>
2020-12-22 11:00:00 +0900

    upload hivephotoview.js

---
+++ js/hivephotoview.js
@@ -0,0 +1,16 @@
+﻿// 최신 제이쿼리에서 live()함수 실행
+jQuery.fn.extend({
+    live: function (event, callback) {
+        if (this.selector) {
+            jQuery(document).on(event, this.selector, callback);
+        }
+        return this;
+    }
+});
+
+// href="#" 동작 제거
+$(function () {
+    $('.pview_close a[href="#"]').click(function (e) {
+        e.preventDefault();
+    });
+});
