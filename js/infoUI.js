7f004353ee8ccf68be0a53ddb9a198ed3428612c
Bright Chung <bywoch@gmail.com>
2021-01-14 15:00:00 +0900

    update js

--- js/infoUI.js
+++ js/infoUI.js
@@ -2,13 +2,13 @@
     NoticeList: function (o) {
         // 빈 배열 azHtml을 선언 테이블의 내용을 이 배열에 저장
         var azHtml = [];
-    
+
         // _this 변수에 현재 객체(this)를 할당 (현재 객체는 이 함수를 포함한 infoUtil 객체)
         var _this = this;
-    
+
         // _that 변수에 infoUtil 객체를 할당
         var _that = infoUtil;
-    
+
         // 공지사항 정보가 담긴 배열 o의 길이가 0보다 큰 경우
         if (o.length > 0) {
             // 배열 o를 순회하며 테이블의 각 행을 생성하여 azHtml 배열에 추가
@@ -19,12 +19,12 @@
                 azHtml.push('<div class="in">');
                 // 제목과 해당 공지사항의 링크를 추가
                 azHtml.push('<div class="t_tit"><a href="' + _that.PageNoticeView + o[i].code + '&page=' + _that.Page + '&opt=' + _that.Opt + '&keyword=' + encodeURIComponent(_that.Keyword) + '"><strong>' + o[i].title + '</strong></a>');
-    
+
                 // 공지사항이 새로운 항목인 경우, "new" 아이콘을 추가
                 if (o[i].is_new) {
                     azHtml.push('<span class="sp_tinfo ico_new">new</span>');
                 }
-    
+
                 azHtml.push('</div>');
                 azHtml.push('</div>');
                 azHtml.push('</td>');
@@ -38,10 +38,10 @@
             // 검색된 결과가 없는 경우, "검색된 결과가 없습니다."라는 행을 생성하여 azHtml 배열에 추가
             azHtml.push('<tr><td colspan="3"><div class="in"><div class="t_tit"><strong>검색된 결과가 없습니다.</strong></div></div></td></tr>');
         }
-    
+
         // 생성한 테이블 내용을 웹 페이지의 특정 요소에 삽입
         $(".sct_tblarea").find("tbody").html(azHtml.join(''));
-    
+
         // _this 객체의 SetPager 메서드를 호출하여 페이징 처리를 수행
         _this.SetPager();
 
@@ -147,41 +147,63 @@
         var _this = this;
         var _that = infoUtil;
         var k = 4;
+
         var title = "";
         var _idxCnt = -1;
+
+        // 배열 'o'를 순회하며 사진 목록을 생성
         for (var i = 0; i < o.length; i++) {
+            // _that 객체의 Idx 값과 현재 사진의 코드가 일치하는 경우, 해당 사진의 인덱스를 _idxCnt에 저장
             if (_that.Idx == o[i].code) {
                 _idxCnt = i;
             }
 
+            // 한 행에 k개의 사진이 표시되는 경우, 사진 묶음을 시작하는 HTML 코드를 배열 'azHtml'에 추가
             if (i % k == 0) {
                 azHtml.push('<div class="photo_article">');
                 azHtml.push('<ul class="pa_list">');
             }
+
             azHtml.push('<li class="new">');
+
+            // 사진의 경로가 없는 경우, 빈 사진 링크를 추가
             if (o[i].photo_fullpath == "") {
                 azHtml.push('<a class="pic" href="#"></a>');
             } else {
+                // 사진의 경로가 있는 경우, 해당 사진과 타이틀을 추가
                 azHtml.push('<a class="pic" href="#"><img alt="' + o[i].title + '" src="' + _that.ImageServer + o[i].photo_fullpath + '"><span class="blind">' + i + '|' + o[i].code + '</span></a>');
             }
-            azHtml.push('<span class="txt"><span class="cc_ellip" style="min-height: 38px;">' + o[i].title.cut(60) + '</span></span>')
-            azHtml.push('<span style="text-align: right; font-size:11px; margin-top: -18px; margin-right: 7px; display: block; position: relative;">' + o[i].reg_dt_short.substring(2, 10) + '</span>')
+
+            azHtml.push('<span class="txt"><span class="cc_ellip" style="min-height: 38px;">' + o[i].title.cut(60) + '</span></span>');
+
+            // 사진의 등록 날짜를 오른쪽 아래에 표시
+            azHtml.push('<span style="text-align: right; font-size:11px; margin-top: -18px; margin-right: 7px; display: block; position: relative;">' + o[i].reg_dt_short.substring(2, 10) + '</span>');
+
+            // 사진이 새로운 항목인 경우, "new" 아이콘 추가
             if (o[i].is_new) {
                 azHtml.push('<span class="sp_tinfo ico_new">new</span>');
             }
+
             azHtml.push('</span></span>');
             azHtml.push('	</li>');
 
+            // 한 행에 k개의 사진이 표시되는 경우, 사진 묶음을 닫는 HTML 코드를 배열 'azHtml'에 추가
             if (i % k == k - 1) {
                 azHtml.push('</ul>');
                 azHtml.push('</div>');
             }
-
         }
+
+        // 생성한 사진 목록을 사진 갤러리 요소에 표시
         $(".photo_wrap").html(azHtml.join(''));
+
+        // 페이저 설정 함수를 호출하여 사진 갤러리의 페이저를 설정
         _this.SetPager();
+
+        // 뷰어 설정 함수를 호출하여 사진 뷰어를 설정
         _this.SetViewer();
 
+        // _idxCnt가 0 이상인 경우, 해당 인덱스의 사진을 클릭
         if (_idxCnt >= 0) {
             $(".photo_wrap").find("li").eq(_idxCnt).find("a").click();
         }
@@ -332,25 +354,38 @@
             });
         });
     },
+    // 페이지 전환 함수 페이지 번호(p)를 인자로 받아옴
     ChangePage: function (p) {
+        // _this 변수에 현재 객체(this)를 할당 (현재 객체는 이 함수를 포함한 infoUtil 객체)
         var _this = this;
+
+        // _that 변수에 infoUtil 객체를 할당
         var _that = infoUtil;
+
+        // 전달받은 페이지 번호(p)를 _that 객체의 Page에 저장
         _that.Page = p;
+
+        // _that 객체의 Location 값에 따라 다른 목록을 가져와 페이지를 변경
         switch (_that.Location) {
             case "Notice":
+                // 공지사항 목록을 가져와 페이지를 변경
                 _that.NoticeList();
                 break;
             case "Press":
+                // 보도자료 목록을 가져와 페이지를 변경
                 _that.PressList();
                 break;
             case "Photo":
+                // 사진 목록을 가져와 페이지를 변경
                 _that.PhotoList();
                 break;
             case "NewsLetter":
+                // 뉴스레터 목록을 가져와 페이지를 변경
                 _that.NewsLetterList();
                 break;
         }
     }
+
 }
 
 // 문자열을 지정된 길이(len)로 자르는 메서드
@@ -371,7 +406,7 @@ String.prototype.cut = function (len) {
 }
 
 
-// 문자열의 바이트 수를 계산하는 메서드입니다.
+// 문자열의 바이트 수를 계산하는 메서드
 String.prototype.bytes = function () {
     var str = this; // 메서드를 호출한 문자열을 변수 str에 저장
     var l = 0; // 문자열의 길이를 저장하는 변수 l을 초기화
